5a9d4330fad75b2e11eca3756e6341a462d3a1fc
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-10-06 19:38:14 -0300

    feat: menu open and close feature

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,17 @@
 import Logo from "../Logo/Logo";
 import styles from "./Menu.module.sass";
+import React from "react";
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  console.log(isOpen);
   return (
     <header className={styles.header}>
       <div className={styles.menu_controller}>
-        <div className={styles.menu_icon}>
+        <div
+          className={`${styles.menu_icon} ${isOpen ? styles.open : ""}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <span></span>
           <span></span>
           <span></span>
@@ -14,14 +20,24 @@ const Menu = () => {
       <div className={styles.logo_wrapper}>
         <Logo />
       </div>
-      <div className={styles.nav_container}>
+      <div
+        className={`${styles.nav_container} ${
+          isOpen ? styles.enabled : styles.disabled
+        }`}
+      >
         <nav className={styles.nav}>
           <ul>
             <li>
               <a>Home</a>
             </li>
             <li>
-              <a>Cardápio</a>
+              <a>Cardápio principal</a>
+            </li>
+            <li>
+              <a>Bebidas</a>
+            </li>
+            <li>
+              <a>Sobremesas</a>
             </li>
             <li className={styles.button_li}>
               <a className={styles.button}>Marmitas</a>
